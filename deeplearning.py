f73cda9b11cf3440b914faa608463d667861938e
xun6000 <xun6000@gmail.com>
2017-09-11 23:09:52 -0500

    I add an "a" to test the commit

--- deeplearning.py
+++ deeplearning.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
-
+a =3
 circle1 = plt.Circle((0, 0), 0.2, color='r')
 circle2 = plt.Circle((0.5, 0.5), 0.2, color='blue')
 circle3 = plt.Circle((1, 1), 0.2, color='g', clip_on=False)
-
+print  a
 fig, ax = plt.subplots() # note we must use plt.subplots, not plt.subplot
 # (or if you have an existing figure)
 # fig = plt.gcf()
